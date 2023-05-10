284c947c4380dcc109ae2a1689f46c8899baddc0
VinsmokeSans <sandi.febrian26@gmail.com>
2023-05-10 10:03:25 +0700

    first commit

---
+++ app/Views/index.php
@@ -0,0 +1,479 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>CV Sandi Reza</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="<?php base_url();?>/Asset/Personal/assets/img/favicon.png" rel="icon">
+  <link href="<?php base_url();?>/Asset/Personal/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="<?php base_url();?>/Asset/Personal/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="<?php base_url();?>/Asset/Personal/assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container">
+
+      <h1><a href="">Sandi Reza</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+      <h2>I'm a <span>Web Developer</span> from Sukabumi</h2>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Home</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#resume">Resume</a></li>
+          <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="social-links">
+        <a href="https://web.facebook.com/sandi.reza.1806/" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/_sandireza/" class="instagram"><i class="bi bi-instagram"></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>Learn more about me</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="<?php base_url();?>/Asset/Personal/assets/img/SandiReza.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Web Designer &amp; Developer</h3>
+          <p class="fst-italic">
+            "Live by your principles not by what other people say"
+          </p>
+          <br>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Name:</strong> <span>Sandi Reza Febrian</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>February 2002</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>08xx xxxxx xx67</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Sukabumi, Indonesia</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span><?php  $year = date("Y"); $birth=$year-2002; echo $birth?></span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>-</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>E-mail:</strong> <span>sandi.febrian26@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+              </ul>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
+
+    <!-- ======= Skills  ======= -->
+    <div class="skills container">
+
+      <div class="section-title">
+        <h2>Skills</h2>
+      </div>
+
+      <div class="row skills-content">
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">HTML <i class="val">Intermediate</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">CSS <i class="val">Elementary</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">JavaScript <i class="val">Beginner</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">PHP <i class="val">Elementary</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">Java <i class="val">Intermediate</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">Photoshop <i class="val">Elementary</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div><!-- End Skills -->
+
+    <!-- ======= Hobbies ======= -->
+    <div class="interests container">
+
+      <div class="section-title">
+        <h2>Hobbies</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-4">
+          <div class="icon-box">
+            <i class="ri-camera-line" style="color: #ffbb2c;"></i>
+            <h3>Photograper</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+            <i class="ri-basketball-line" style="color: #5578ff;"></i>
+            <h3>Basketball</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+            <i class="ri-football-line" style="color: #e80368;"></i>
+            <h3>Soccer</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+          <div class="icon-box">
+            <i class="ri-headphone-line" style="color: #e361ff;"></i>
+            <h3>Music</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="ri-run-line" style="color: #47aeff;"></i>
+            <h3>Jogging</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="ri-movie-line" style="color: #ffa76e;"></i>
+            <h3>Cinema</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="ri-palette-line" style="color: #11dbcf;"></i>
+            <h3>Drawing</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="ri-plane-line" style="color: #4233ff;"></i>
+            <h3>Holidays</h3>
+          </div>
+        </div>
+        
+    </div><!-- End Interests -->
+
+  </section><!-- End About Section -->
+
+  <!-- ======= Resume Section ======= -->
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Resume</h2>
+        <p>Check My Resume</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <h3 class="resume-title">Sumary</h3>
+          <div class="resume-item pb-0">
+            <h4>PT Terus Jaya</h4>
+            <p><em>working in a deadline driven online parts sales department with 3 years experience as well as web development accompanied by lectures gave a very great impression.</em></p>
+            <p>
+            <ul>
+            <li>Jl. Pelabuhan 2 No.243 Sukabumi</li>
+              <li>(0266) 227700</li>
+              <li>terusjaya@gmail.com</li>
+            </ul>
+            </p>
+          </div>
+
+          <h3 class="resume-title">Education</h3>
+          <div class="resume-item">
+            <h4>SDN Cibeureum Hilir 5</h4>
+            <h5>2008 - 2014</h5>
+        </div>
+          <div class="resume-item">
+            <h4>SMPN 5 Sukabumi</h4>
+            <h5>2014 - 2017</h5>
+         </div>
+          <div class="resume-item">
+            <h4>SMKN 2 Sukabumi</h4>
+            <h5>2017 - 2020</h5>
+        </div>
+        </div>
+        <div class="col-lg-6">
+          <h3 class="resume-title">Experience</h3>
+          <div class="resume-item">
+            <h4>Apprenticeship</h4>
+            <h5>2019</h5>
+            <p><em>PT Inti, Bandung </em></p>
+            <p>
+            Creating office administration applications such as incoming and outgoing mail using the Java programming language and Netbeans software. desktop based application, application development is 3 months
+            </p>
+          </div>
+          <div class="resume-item">
+            <h4>Working Experience</h4>
+            <h5>2020 - 2023</h5>
+            <p><em>PT Terus Jaya, Sukabumi</em></p>
+            <p>
+            Working in the online parts sales department for 3 years made me knowledgeable about the job situation. responsibility and perseverance are needed at work, I finished working at this company because I had to focus on finishing my college
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Resume Section -->
+
+  <!-- ======= Portfolio Section ======= -->
+  <section id="portfolio" class="portfolio">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Portfolio</h2>
+        <p>My Works</p>
+      </div>
+
+      <div class="row portfolio-container">
+
+        <div class="col-lg-4 col-md-6 portfolio-item ">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Penggajian PT. Terus Jaya Motor</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Penggajian"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/gudangSparepart.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Gudang Sparepart</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/gudangSparepart.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Gudang Sparepart"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item ">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-3.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Penjualan Sparepart</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-3.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Penjualan Sparepart"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-4.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Laundry</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-4.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Laundry"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-5.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Absensi Karyawan</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Absensi"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item ">
+          <div class="portfolio-wrap">
+            <img src="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
+            <div class="portfolio-info">
+              <h4>Aplikasi Retail</h4>
+              <p>Web</p>
+              <div class="portfolio-links">
+                <a href="<?php base_url();?>/Asset/Personal/assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Aplikasi Retail"><i class="bx bx-plus"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+
+      </div>
+
+    </div>
+  </section><!-- End Portfolio Section -->
+
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p>Contact Me</p>
+      </div>
+
+      <div class="row mt-2">
+
+        <div class="col-md-6 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-map"></i>
+            <h3>My Address</h3>
+            <p>Jl Baru Sukaraja, Sukabumi</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-share-alt"></i>
+            <h3>Social Profiles</h3>
+            <div class="social-links">
+                <a href="https://web.facebook.com/sandi.reza.1806/" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="https://www.instagram.com/_sandireza/" class="instagram"><i class="bi bi-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-envelope"></i>
+            <h3>Email Me</h3>
+            <p>sandi.febrian26@gmail.com</p>
+          </div>
+        </div>
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-phone-call"></i>
+            <h3>Call Me</h3>
+            <p>08xx xxxxx xx67</p>
+          </div>
+        </div>
+      </div>
+
+      <form action="#" method="post" role="form" class="php-email-form mt-4">
+        <div class="row">
+          <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+          </div>
+          <div class="col-md-6 form-group mt-3 mt-md-0">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+          </div>
+        </div>
+        <div class="form-group mt-3">
+          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+        </div>
+        <div class="form-group mt-3">
+          <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+        </div>
+        <div class="my-3">
+          <div class="loading">Loading</div>
+          <div class="error-message">Sorry, this website is under maintenance</div>
+          <div class="sent-message">Your message has been sent. Thank you!</div>
+        </div>
+        <div class="text-center"><button type="submit">Send Message</button></div>
+      </form>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+  <div class="credits">
+    <p class="m-0">&copy; Sandi Reza 2023</p>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="<?php base_url();?>/Asset/personalssets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="<?php base_url();?>/Asset/Personal/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="<?php base_url();?>/Asset/Personal/assets/js/main.js"></script>
+
+</body>
+
+</html>
